f0d8e5acb53c4c06547a685f283d51da18f86cd4
irevoire <irevoire@protonmail.ch>
2021-02-14 00:57:43 +0100

    [3.5] vec3 Utility Functions

--- in_one_weekend/src/vec3.rs
+++ in_one_weekend/src/vec3.rs
@@ -21,9 +21,25 @@ impl Vec3 {
     pub fn length_squared(&self) -> f64 {
         self.0 * self.0 + self.1 * self.1 + self.2 * self.2
     }
+
+    pub fn dot(&self, other: &Self) -> f64 {
+        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
+    }
+
+    pub fn cross(&self, other: &Self) -> Self {
+        Self(
+            self.1 * other.2 - self.2 * other.1,
+            self.2 * other.0 - self.0 * other.2,
+            self.0 * other.1 - self.1 * other.0,
+        )
+    }
+
+    pub fn unit_vector(&self) -> Self {
+        self / self.length()
+    }
 }
 
-use core::ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg};
+use core::ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub};
 
 impl Neg for Vec3 {
     type Output = Self;
@@ -73,6 +89,14 @@ impl Add for Vec3 {
     }
 }
 
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
 impl MulAssign for Vec3 {
     fn mul_assign(&mut self, other: Self) {
         self.0 *= other.0;
@@ -108,3 +132,41 @@ impl Div for Vec3 {
 // Type aliases for vec3
 type Point3 = Vec3; // 3D point
 type Color = Vec3; // RGB color
+
+impl std::fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.0, self.1, self.2)
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self::Output {
+        Self(self.0 * other, self.1 * other, self.2 * other)
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, other: Vec3) -> Self::Output {
+        Vec3(self * other.0, self * other.1, self * other.2)
+    }
+}
+
+impl Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, other: f64) -> Self::Output {
+        Vec3(self.0 / other, self.1 / other, self.2 / other)
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: f64) -> Self::Output {
+        Self(self.0 / other, self.1 / other, self.2 / other)
+    }
+}
