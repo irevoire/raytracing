dc3eef5d1b08f0f121f84462f9073fa973ab96fc
irevoire <irevoire@protonmail.ch>
2021-02-14 01:45:45 +0100

    [4.8] Sending Rays Into the Scene

--- in_one_weekend/src/main.rs
+++ in_one_weekend/src/main.rs
@@ -2,14 +2,33 @@ mod color;
 mod ray;
 mod vec3;
 
+use ray::Ray;
 pub use vec3::{Color, Vec3};
 
+fn ray_color(r: Ray) -> Color {
+    let unit_direction = r.dir.unit_vector();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+
+    (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+}
+
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as usize;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vec3(0., 0., 0.);
+    let horizontal = Vec3(viewport_width, 0., 0.);
+    let vertical = Vec3(0., viewport_height, 0.);
+    let lower_left_corner = origin - horizontal / 2. - vertical / 2. - Vec3(0., 0., focal_length);
 
     // Render
-
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
@@ -17,11 +36,14 @@ fn main() {
     for j in (0..image_height).rev() {
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..image_width {
-            let pixel_color = Vec3(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.25,
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray_color(r);
 
             color::write_color(pixel_color);
         }
