cf59cc648a1bf83df79291495504e4f4683d5317
irevoire <irevoire@protonmail.ch>
2021-02-14 13:56:26 +0100

    [8.22] Using Gamma Correction for Accurate Color Intensity

--- in_one_weekend/src/color.rs
+++ in_one_weekend/src/color.rs
@@ -5,11 +5,11 @@ pub fn write_color(pixel_color: Color, samples_per_pixel: usize) {
     let mut g = pixel_color.y();
     let mut b = pixel_color.z();
 
-    // Divid the color by the number of samples.
+    // Divid the color by the number of samples and gamma-correct for gamma=2.0
     let scale = 1. / samples_per_pixel as f64;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = (scale * r).sqrt();
+    g = (scale * g).sqrt();
+    b = (scale * b).sqrt();
 
     println!(
         "{} {} {}",
