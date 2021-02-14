aeffed04f89d2b0b64f502b3d909a72903b6b8eb
irevoire <irevoire@protonmail.ch>
2021-02-14 02:25:11 +0100

    [6.13] An Abstraction for Hittable Objects

---
+++ in_one_weekend/src/hittable.rs
@@ -0,0 +1,13 @@
+use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
+
+#[derive(Debug, Clone)]
+pub struct HitRecord {
+    pub p: Point3,
+    pub normal: Vec3,
+    pub t: f64,
+}
+
+pub trait Hittable {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
+}
---
+++ in_one_weekend/src/sphere.rs
@@ -0,0 +1,45 @@
+use crate::hittable::{HitRecord, Hittable};
+use crate::ray::Ray;
+use crate::vec3::*;
+
+#[derive(Debug, Clone)]
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let oc = r.orig - self.center;
+        let a = r.dir.length_squared();
+        let half_b = oc.dot(&r.dir);
+        let c = oc.length_squared() - self.radius * self.radius;
+
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0. {
+            return false;
+        }
+        let sqrtd = discriminant.sqrt();
+
+        // Find the nearest root that lies in the acceptable range.
+        let mut root = (-half_b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = (rec.p - self.center) / self.radius;
+
+        true
+    }
+}
