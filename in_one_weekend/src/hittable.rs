e993bad2fc3605ae690cddfc0ad5a24c18c59623
irevoire <irevoire@protonmail.ch>
2021-02-14 02:33:25 +0100

    [6.14] Front Faces Versus Back Faces

--- in_one_weekend/src/hittable.rs
+++ in_one_weekend/src/hittable.rs
@@ -6,6 +6,18 @@ pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        self.front_face = r.dir.dot(outward_normal) < 0.;
+        self.normal = if self.front_face {
+            *outward_normal
+        } else {
+            -*outward_normal
+        };
+    }
 }
 
 pub trait Hittable {
