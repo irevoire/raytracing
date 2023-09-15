a4cd1aef2fca3e4750269651a36cf25ec5aa8898
Tamo <tamo@meilisearch.com>
2023-09-15 15:22:57 +0200

    the ppm image format

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.;
+
+            let ir = (255.999 * r) as u64;
+            let ig = (255.999 * g) as u64;
+            let ib = (255.999 * b) as u64;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
+}
