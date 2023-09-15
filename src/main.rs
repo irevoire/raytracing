d040fdf23089d27d450b989c7203b5ee44a555aa
Tamo <tamo@meilisearch.com>
2023-09-15 17:39:34 +0200

    Sending Rays Into the Scene

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-use raytracing::{write_color, Color};
+use raytracing::{write_color, Color, Point3, Ray, Vec3};
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    let image_height = (image_width as f64 / aspect_ratio).ceil() as usize;
+
+    // Camera
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let camera_center = Point3::new();
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    let viewport_u = Vec3::from(viewport_width, 0, 0);
+    let viewport_v = Vec3::from(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    // Calculate the location of the upper left pixel.
+    let viewport_upper_left =
+        camera_center - Vec3::from(0, 0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
@@ -13,13 +35,19 @@ fn main() {
     for j in 0..image_height {
         eprintln!("Scanline remaining: {}", image_height - j);
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b = 0.;
-
-            let color = Color::from(r, g, b);
+            let pixel_center =
+                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
+            let color = ray_color(&r);
             write_color(color);
         }
     }
     eprintln!("done");
+}
+
+pub fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().unit_vector();
+    let a = 0.5 * unit_direction.y() + 1.0;
+    (1.0 - a) * Color::from(1.0, 1.0, 1.0) + a * Color::from(0.5, 0.7, 1.0)
 }
