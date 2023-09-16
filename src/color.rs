6b12ab067d21dd2ce89a6e6f1c7a6c7b52ed0ecf
Tamo <tamo@meilisearch.com>
2023-09-16 13:12:17 +0200

    Using Gamma Correction for Accurate Color Intensity

--- src/color.rs
+++ src/color.rs
@@ -1,12 +1,16 @@
 use crate::{Color, Interval};
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    return linear_component.sqrt();
+}
+
 pub fn write_color(color: Color, sample_per_pixel: usize) {
     // Divide the color by the number of samples.
     let scale = 1. / sample_per_pixel as f64;
 
-    let r = color.r() * scale;
-    let g = color.g() * scale;
-    let b = color.b() * scale;
+    let r = linear_to_gamma(color.r() * scale);
+    let g = linear_to_gamma(color.g() * scale);
+    let b = linear_to_gamma(color.b() * scale);
 
     // Write the translated [0,255] value of each color component.
     let intensity = Interval::from(0., 1. - f64::EPSILON);
