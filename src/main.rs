6c00db5e85f86729711385a2e098a206e1fc8d72
Tamo <tamo@meilisearch.com>
2023-09-16 15:33:45 +0200

    Schlick Approximation: first bug?

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ fn main() {
     let material_ground = Arc::new(material::Lambertian::new(Color::from(0.8, 0.8, 0.0)));
     let material_center = Arc::new(material::Lambertian::new(Color::from(0.1, 0.2, 0.5)));
     let material_left = Arc::new(material::Dielectric::new(1.5));
-    let material_right = Arc::new(material::Metal::new(Color::from(0.8, 0.6, 0.2), 1.));
+    let material_right = Arc::new(material::Metal::new(Color::from(0.8, 0.6, 0.2), 0.));
 
     #[rustfmt::skip]
     {
         world.add(Arc::new(Sphere::new(Point3::from( 0.0, -100.5, -1.0), 100.0, material_ground)));
         world.add(Arc::new(Sphere::new(Point3::from( 0.0,    0.0, -1.0),   0.5, material_center)));
-        world.add(Arc::new(Sphere::new(Point3::from(-1.0,    0.0, -1.0),   0.5, material_left)));
+        world.add(Arc::new(Sphere::new(Point3::from(-1.0,    0.0, -1.0),   -0.4, material_left)));
         world.add(Arc::new(Sphere::new(Point3::from( 1.0,    0.0, -1.0),   0.5, material_right)));
     };
 
--- src/material.rs
+++ src/material.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::{Color, HitRecord, Ray, Vec3};
 
 pub trait Material {
@@ -79,6 +81,13 @@ impl Dielectric {
             ir: index_of_refraction,
         }
     }
+
+    pub fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        // Use Schlick's approximation for reflectance.
+        let mut r0 = (1. - ref_idx) / (1. + ref_idx);
+        r0 = r0 * r0;
+        r0 + (1. - r0) * (1. - cosine).powi(5)
+    }
 }
 
 impl Material for Dielectric {
@@ -104,7 +113,8 @@ impl Material for Dielectric {
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
         let direction;
 
-        if cannot_refract {
+        let mut rng = rand::thread_rng();
+        if cannot_refract || Self::reflectance(cos_theta, refraction_ratio) > rng.gen::<f64>() {
             direction = unit_direction.reflect_with(rec.normal);
         } else {
             direction = unit_direction.refract_with(rec.normal, refraction_ratio);
